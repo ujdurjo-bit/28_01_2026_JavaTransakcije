ed1acc3e88bf19bb03e6090fe2153c7774cd4cb5
Josip <ujdur.jo@gmail.com>
2026-01-28 18:46:04 +0100

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -3,35 +3,40 @@ import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Savepoint;
 import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        DataSource dS = createDataSource();
+        DataSource dataSource = createDataSource();
+        Savepoint savepoint = null;
 
-        try (Connection connection = dS.getConnection()) {
+        try (Connection connection = dataSource.getConnection()) {
             System.out.println("Uspješno spajanje na bazu!");
 
-            //try-catch blockza izvršavanje transakcije
-            try (Statement stmnt1 = connection.createStatement();
-                Statement stmnt2 = connection.createStatement()) {
+            //try-catch blok za izvršavanje transakcije
+            try (Statement stat1 = connection.createStatement(); Statement stat2 = connection.createStatement()) {
                 connection.setAutoCommit(false); //isključujemo automatski commit transakcije
-                stmnt1.executeUpdate("INSERT INTO Drzava (Naziv) VALUES ('Indija')");
-                stmnt2.executeUpdate("UPDATE Drzava SET Naziv = 'Croatia' WHERE IDDrzava = "); //greška
+                stat1.executeUpdate("INSERT INTO Drzava (Naziv) VALUES ('Mađarska')");
+                savepoint = connection.setSavepoint("kontrolnatocka"); //kreiranje kontrolne točke
+                stat2.executeUpdate("UPDATE Drzava SET Naziv = 'Croatia' WHERE IDDrzava = "); //greška
 
                 connection.commit();
                 System.out.println("Transakcija izvršena!");
-
             } catch (SQLException e) {
-                System.err.println("Transakcija poništena!");
-                connection.rollback();
+//                System.err.println("Transakcija poništena!");
+//                connection.rollback();
+
+                System.err.println("Transakcija dijelom poništena!");
+                try {
+                    connection.rollback(savepoint);
+                    connection.commit();
+                    System.out.println("Transakcija vraćena na kontrolnu točku!");
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
 
             }
-
-
-
-
-
 
         } catch (SQLException e) {
             System.err.println("Greška prilikom spajanja na bazu!");
@@ -39,8 +44,8 @@ public class Main {
 
         }
 
-
     }
+
 
     //metoda spajanje na bazu
     private static DataSource createDataSource() {
